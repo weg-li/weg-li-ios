91b500516cfee3b13f1e88976f193ff212cc58f5
Stefan Trauth <mail@stefantrauth.de>
2019-10-08 08:20:47 +0200

    create empty SwiftUI project

---
+++ Wegli/Wegli/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wegli
+//
+//  Created by Stefan Trauth on 08.10.19.
+//  Copyright © 2019 Stefan Trauth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
