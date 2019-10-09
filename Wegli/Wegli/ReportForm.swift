d87bcd648ad5931f861336bf1f97f4ebe33b8fa3
Stefan Trauth <mail@stefantrauth.de>
2019-10-09 14:34:10 +0200

    use scrollview to have enough space

--- Wegli/Wegli/ReportForm.swift
+++ Wegli/Wegli/ReportForm.swift
@@ -10,13 +10,15 @@ import SwiftUI
 
 struct ReportForm: View {
     var body: some View {
-        VStack {
-            Widget(title: Text("Fotos"), state: .completed, content: AnyView(Images()))
-            Widget(title: Text("Ort"), state: .completed, content: AnyView(Location()))
-            Widget(title: Text("Beschreibung"), state: .completed, content: AnyView(Description()))
-            Widget(title: Text("Persönliche Daten"), state: .completed, content: AnyView(PersonalData()))
-            SubmitButton(state: .readyToSubmit(ordnungsamt: "München")) {}
-            DiscardButton() {}
+        ScrollView {
+            VStack {
+                Widget(title: Text("Fotos"), state: .completed, content: AnyView(Images()))
+                Widget(title: Text("Ort"), state: .completed, content: AnyView(Location()))
+                Widget(title: Text("Beschreibung"), state: .completed, content: AnyView(Description()))
+                Widget(title: Text("Persönliche Daten"), state: .completed, content: AnyView(PersonalData()))
+                SubmitButton(state: .readyToSubmit(ordnungsamt: "München")) {}
+                DiscardButton() {}
+            }
         }
     }
 }
