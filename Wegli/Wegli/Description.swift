c02453331807c3b61f092d13f2a725ecaf7326c8
Stefan Trauth <mail@stefantrauth.de>
2019-10-09 13:53:31 +0200

    create views for all widget contents

---
+++ Wegli/Wegli/Description.swift
@@ -0,0 +1,21 @@
+//
+//  Description.swift
+//  Wegli
+//
+//  Created by Stefan Trauth on 09.10.19.
+//  Copyright © 2019 Stefan Trauth. All rights reserved.
+//
+
+import SwiftUI
+
+struct Description: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Description_Previews: PreviewProvider {
+    static var previews: some View {
+        Description()
+    }
+}
---
+++ Wegli/Wegli/PersonalData.swift
@@ -0,0 +1,21 @@
+//
+//  PersonalData.swift
+//  Wegli
+//
+//  Created by Stefan Trauth on 09.10.19.
+//  Copyright © 2019 Stefan Trauth. All rights reserved.
+//
+
+import SwiftUI
+
+struct PersonalData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonalData_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalData()
+    }
+}
