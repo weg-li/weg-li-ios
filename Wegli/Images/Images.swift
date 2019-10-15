342f4aabbbf2712978028e6dbc8778e31aa071e5
Stefan Trauth <mail@stefantrauth.de>
2019-10-15 15:09:46 +0200

    move all image widget files into separate folder

---
+++ Wegli/Images/Images.swift
@@ -0,0 +1,23 @@
+//
+//  Images.swift
+//  Wegli
+//
+//  Created by Stefan Trauth on 09.10.19.
+//  Copyright © 2019 Stefan Trauth. All rights reserved.
+//
+
+import SwiftUI
+
+struct Images: View {
+    @ObservedObject var imageDataStore = ImageDataStore()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Images_Previews: PreviewProvider {
+    static var previews: some View {
+        Images()
+    }
+}
