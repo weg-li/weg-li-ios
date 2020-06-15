a6e109dd62de8ce72de4ba225dbeea48a0d4a76c
Malte Bünz <malte.buenz@deutschebahn.com>
2020-06-15 11:31:46 +0200

    extend appState with report types

--- Wegli/App/Redux/AppState.swift
+++ Wegli/App/Redux/AppState.swift
@@ -15,13 +15,29 @@ import UIKit
 struct Report {
     var images: [UIImage] = []
     var address: CNPostalAddress?
+    
+    // MARK: Description
+    struct Car {
+        var color: String?
+        var type: String?
+        var licensePlateNumber: String?
+    }
+    var car = Car()
+    
+    struct Crime {
+        var duration: String?
+        var type: String?
+        var blockedOthers = false
+    }
+    var crime = Crime()
 }
 
-struct LocationState {
-    var isAuthorized: Bool = false
-    var userDefinedLocation: CLLocationCoordinate2D?
-    var location: CLLocationCoordinate2D = CLLocationCoordinate2D()
-    var presumedAddress: CNPostalAddress?
+extension Report {
+    var isDescriptionValid: Bool {
+        ![car.type, car.color, car.licensePlateNumber, crime.type, crime.duration]
+            .compactMap { $0 }
+            .isEmpty
+    }
 }
 
 struct AppState {
@@ -29,4 +45,19 @@ struct AppState {
     var report: Report
     
     var location: LocationState
+}
+
+extension AppState {
+    struct LocationState {
+        var isAuthorized: Bool = false
+        var userDefinedLocation: CLLocationCoordinate2D?
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2D()
+        var presumedAddress: CNPostalAddress?
+    }
+}
+
+extension AppState {
+    struct ViewRoutingState {
+        
+    }
 }
