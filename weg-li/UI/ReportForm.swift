516dc09c3e726e8e01c69a0365e3f3a85df5322d
Malte Bünz <maltebuenz@posteo.de>
2021-03-10 20:42:53 +0100

    update report form

--- weg-li/UI/ReportForm.swift
+++ weg-li/UI/ReportForm.swift
@@ -14,36 +14,34 @@ struct ReportForm: View {
     @State private var editDescription = false
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    Widget(
-                        title: Text("Fotos"),
-                        isCompleted: !store.state.report.images.isEmpty) {
-                            Images()
-                    }
-                    Widget(
-                        title: Text("Ort"),
-                        isCompleted: store.state.location.location != .zero) {
-                            Location()
-                    }
-                    Widget(
-                        title: Text("Beschreibung"),
-                        isCompleted: store.state.report.isDescriptionValid) {
-                            DescriptionWidgetView().environmentObject(self.store)
-                    }
-                    Widget(
-                        title: Text("Persönliche Daten"),
-                        isCompleted: store.state.contact?.isValid ?? false) {
-                            PersonalDataWidget(contact: self.store.state.contact)
-                    }
-                    MailContentView()
-                        .padding([.top, .bottom], 16)
+        ScrollView {
+            VStack {
+                Widget(
+                    title: Text("Fotos"),
+                    isCompleted: !store.state.report.images.isEmpty) {
+                    Images()
                 }
+                Widget(
+                    title: Text("Ort"),
+                    isCompleted: store.state.location.location != .zero) {
+                    Location()
+                }
+                Widget(
+                    title: Text("Beschreibung"),
+                    isCompleted: store.state.report.isDescriptionValid) {
+                    DescriptionWidgetView().environmentObject(self.store)
+                }
+                Widget(
+                    title: Text("Persönliche Daten"),
+                    isCompleted: store.state.contact?.isValid ?? false) {
+                    PersonalDataWidget(contact: self.store.state.contact)
+                }
+                MailContentView()
+                    .padding([.top, .bottom], 16)
             }
-            .padding(.bottom)
-            .navigationBarTitle("Anzeige", displayMode: .inline)
         }
+        .padding(.bottom)
+        .navigationBarTitle("Anzeige", displayMode: .inline)
     }
 }
 
