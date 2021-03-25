e835ef52db692fa907e7a5843d3347194ccb9a82
Malte Bünz <maltebuenz@posteo.de>
2021-03-25 07:45:09 +0100

    remove navigationview and update colors

--- weg-li/Features/Description/UI/EditDescription.swift
+++ weg-li/Features/Description/UI/EditDescription.swift
@@ -41,63 +41,61 @@ struct EditDescription: View {
     }
     
     var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("Fahrzeug")) { // TODO: l18n
-                    TextField(
-                        "Marke", // TODO: l18n
-                        text: viewStore.binding(
-                            get: \.carType,
-                            send:  { ReportAction.car(.type($0)) }
-                        )
+        Form {
+            Section(header: Text("Fahrzeug")) { // TODO: l18n
+                TextField(
+                    "Marke", // TODO: l18n
+                    text: viewStore.binding(
+                        get: \.carType,
+                        send:  { ReportAction.car(.type($0)) }
                     )
-                    TextField(
-                        "Farbe", // TODO: l18n
-                        text: viewStore.binding(
-                            get: \.carColor,
-                            send:  { ReportAction.car(.color($0)) }
-                        )
+                )
+                TextField(
+                    "Farbe", // TODO: l18n
+                    text: viewStore.binding(
+                        get: \.carColor,
+                        send:  { ReportAction.car(.color($0)) }
                     )
-                    TextField(
-                        "Kennzeichen", // TODO: l18n
-                        text: viewStore.binding(
-                            get: \.licensePlate,
-                            send:  { ReportAction.car(.licensePlateNumber($0)) }
-                        )
+                )
+                TextField(
+                    "Kennzeichen", // TODO: l18n
+                    text: viewStore.binding(
+                        get: \.licensePlate,
+                        send:  { ReportAction.car(.licensePlateNumber($0)) }
                     )
-                }
-                .padding(.top, 4)
-                .textFieldStyle(PlainTextFieldStyle())
-                .font(.body)
-                Section(header: Text("Verstoß")) { // TODO: l18n
-                    Picker(
-                        "Art des Verstoßes", // TODO: l18n
-                        selection: viewStore.binding(
-                            get: \.charge.selectedType,
-                            send: { ReportAction.charge(.selectCharge($0)) }
-                        )
-                    ) {
-                        ForEach(0 ..< Report.Charge.charges.count, id: \.self) { chargeIndex in
-                            Text(Report.Charge.charges[chargeIndex])
-                                .tag(chargeIndex)
-                        }
-                    }
-                    Picker(
-                        "Dauer der Verstoßes", // TODO: l18n
-                        selection: viewStore.binding(
-                            get: \.charge.selectedDuration,
-                            send: { ReportAction.charge(.selectDuraration($0)) }
-                        )
-                    ) {
-                        ForEach(0 ..< Times.allCases.count, id: \.self) {
-                            Text(Times.allCases[$0].description)
-                        }
-                    }
-                    toggleRow
-                }.pickerStyle(DefaultPickerStyle())
+                )
             }
-            .navigationBarTitle(Text("Beschreibung"), displayMode: .inline) // TODO: l18n
+            .padding(.top, 4)
+            .textFieldStyle(PlainTextFieldStyle())
+            .font(.body)
+            Section(header: Text("Verstoß")) { // TODO: l18n
+                Picker(
+                    "Art des Verstoßes", // TODO: l18n
+                    selection: viewStore.binding(
+                        get: \.charge.selectedType,
+                        send: { ReportAction.charge(.selectCharge($0)) }
+                    )
+                ) {
+                    ForEach(0 ..< Report.Charge.charges.count, id: \.self) {
+                        Text(Report.Charge.charges[$0])
+                            .tag($0)
+                    }
+                }
+                Picker(
+                    "Dauer der Verstoßes", // TODO: l18n
+                    selection: viewStore.binding(
+                        get: \.charge.selectedDuration,
+                        send: { ReportAction.charge(.selectDuraration($0)) }
+                    )
+                ) {
+                    ForEach(0 ..< Times.allCases.count, id: \.self) {
+                        Text(Times.allCases[$0].description)
+                    }
+                }
+                toggleRow
+            }.pickerStyle(DefaultPickerStyle())
         }
+        .navigationBarTitle(Text("Beschreibung"), displayMode: .inline) // TODO: l18n
     }
         
     private var toggleRow: some View {
@@ -109,7 +107,7 @@ struct EditDescription: View {
                     get: \.blockedOthers,
                     send: { _ in ReportAction.charge(.toggleBlockedOthers) }
                 )
-            )
+            ).animation(.easeIn(duration: 0.1))
         }
     }
 }
