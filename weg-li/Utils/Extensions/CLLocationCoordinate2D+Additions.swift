a3e94277d0dbad0f71469fa76ba23165c0ed086e
Malte Bünz <maltebuenz@posteo.de>
2021-03-28 21:07:23 +0200

    add Codable extension to clllocationCoordinate

--- weg-li/Utils/Extensions/CLLocationCoordinate2D+Additions.swift
+++ weg-li/Utils/Extensions/CLLocationCoordinate2D+Additions.swift
@@ -13,3 +13,18 @@ extension CLLocationCoordinate2D: Equatable {
         lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
 }
+
+extension CLLocationCoordinate2D: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        try container.encode(longitude)
+        try container.encode(latitude)
+    }
+     
+    public init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        let longitude = try container.decode(CLLocationDegrees.self)
+        let latitude = try container.decode(CLLocationDegrees.self)
+        self.init(latitude: latitude, longitude: longitude)
+    }
+}
