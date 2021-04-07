fb59ff842dfd18f88fd7d155339b50e2947d8178
Malte Bünz <maltebuenz@posteo.de>
2021-04-07 11:42:27 +0200

    adopt images view

--- weg-li/Features/Images/ImagesCore.swift
+++ weg-li/Features/Images/ImagesCore.swift
@@ -7,37 +7,55 @@ import UIKit
 
 struct ImagesViewState: Equatable, Codable {
     var showImagePicker: Bool = false
-    var storedPhotos: [StorableImage] = []
-    var resolvedLocation: CLLocationCoordinate2D?
+    var storedPhotos: [StorableImage?] = []
+    var resolvedLocation: CLLocationCoordinate2D = .zero
+    
+    var imageStates: IdentifiedArrayOf<ImageState> {
+        IdentifiedArray(
+            storedPhotos
+                .compactMap { $0 }
+                .map { ImageState(id: $0.id, image: $0) }
+        )
+    }
 }
 
 enum ImagesViewAction: Equatable {
-    case addPhoto(UIImage)
-    case removePhoto(index: Int)
+    case addPhotos([StorableImage?])
     case setShowImagePicker(Bool)
-    case setResolvedCoordinate(CLLocationCoordinate2D?)
+    case setResolvedCoordinate(CLLocationCoordinate2D)
+    case image(id: UUID, action: ImageAction)
 }
 
 struct ImagesViewEnvironment {
     let imageConverter: ImageConverter
+    let distanceFilter: Double = 50
 }
 
-let imagesReducer = Reducer<ImagesViewState, ImagesViewAction, ImagesViewEnvironment> { state, action, _ in
+let imagesReducer = Reducer<ImagesViewState, ImagesViewAction, ImagesViewEnvironment> { state, action, env in
     switch action {
+    case .image(let id, let imageAction):
+        switch imageAction {
+        case .removePhoto:
+            let photos = state.storedPhotos
+                .compactMap{ $0 }
+                .filter { $0.id != id }
+            state.storedPhotos = photos
+            return .none
+        }
     case let .setShowImagePicker(value):
         state.showImagePicker = value
         return .none
-    case let .addPhoto(photo):
-//        let scaledImage = environment.imageConverter.scalePreservingAspectRatio(
-//            image: photo,
-//            targetSize: .init(width: 1000, height: 1000)
-//        )
-        state.storedPhotos.append(StorableImage(uiImage: photo)!)
-        return .none
-    case let .removePhoto(index):
-        state.storedPhotos.remove(at: index)
+    case let .addPhotos(photos):
+        state.storedPhotos = photos
         return .none
     case let .setResolvedCoordinate(coordinate):
+        let resolved = CLLocation(from: state.resolvedLocation)
+        let location = CLLocation(from: coordinate)
+        
+        if resolved.distance(from: location) < env.distanceFilter {
+            return .none
+        }
+        
         state.resolvedLocation = coordinate
         return .none
     }
