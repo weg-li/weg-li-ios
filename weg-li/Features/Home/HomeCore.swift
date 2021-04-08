2ca0dfb82818139beccc2ac46c79e60299337119
Malte Bünz <maltebuenz@posteo.de>
2021-04-08 20:44:19 +0200

    use settingsreducer

--- weg-li/Features/Home/HomeCore.swift
+++ weg-li/Features/Home/HomeCore.swift
@@ -11,12 +11,8 @@ import UIKit
 // MARK: - AppState
 
 struct HomeState: Equatable {
-    /// Users contact data
-    private var _storedContact = ContactState()
-    var contact: ContactState {
-        get { _storedContact }
-        set { _storedContact = newValue }
-    }
+    /// Settings
+    var settings = SettingsState(contact: .empty)
 
     /// Reports a user has sent
     var reports: [Report] = []
@@ -26,7 +22,7 @@ struct HomeState: Equatable {
     var reportDraft: Report {
         get {
             guard let report = _storedReport else {
-                return Report(images: .init(), contact: contact, date: Date.init)
+                return Report(images: .init(), contact: settings.contact, date: Date.init)
             }
             return report
         }
@@ -39,8 +35,9 @@ struct HomeState: Equatable {
 }
 
 // MARK: - AppAction
+
 enum HomeAction: Equatable {
-    case contact(ContactAction)
+    case settings(SettingsAction)
     case report(ReportAction)
     case showReportWizard(Bool)
     case reportSaved
@@ -65,24 +62,24 @@ let homeReducer = Reducer<HomeState, HomeAction, HomeEnvironment>.combine(
                     placeService: PlacesServiceImplementation(),
                     regulatoryOfficeMapper: RegulatoryOfficeMapper(districtsRepo: DistrictRepository()))
             }),
-    contactReducer.pullback(
-        state: \.contact,
-        action: /HomeAction.contact,
-        environment: { _ in ContactEnvironment() }),
+    settingsReducer.pullback(
+        state: \.settings,
+        action: /HomeAction.settings,
+        environment: { _ in SettingsEnvironment(uiApplicationClient: .live) }),
     Reducer { state, action, environment in
         switch action {
         // restore state from userdefaults
         case .onAppear:
             if let contact = environment.userDefaultsClient.contact {
-                state.contact = contact
+                state.settings = SettingsState(contact: contact)
             }
             state.reports = environment.userDefaultsClient.reports
             return .none
-        case let .contact(contactAction):
-            switch contactAction {
-            case .onDisappear:
-                state.reportDraft.contact = state.contact
-                return environment.userDefaultsClient.setContact(state.contact)
+        case let .settings(settingsAction):
+            switch settingsAction {
+            case .contact(.onDisappear):
+                state.reportDraft.contact = state.settings.contact
+                return environment.userDefaultsClient.setContact(state.settings.contact)
                     .fireAndForget()
             default:
                 return .none
@@ -95,7 +92,7 @@ let homeReducer = Reducer<HomeState, HomeAction, HomeEnvironment>.combine(
                 switch mailComposerResult {
                 case .sent:
                     state.reports.append(state.reportDraft)
-                    
+
                     return Effect.concatenate(
                         environment.userDefaultsClient.setReports(state.reports)
                             .fireAndForget(),
@@ -108,13 +105,13 @@ let homeReducer = Reducer<HomeState, HomeAction, HomeEnvironment>.combine(
                 }
             }
             // sync contact with draftReport contact
-            state.contact = state.reportDraft.contact
+            state.settings.contact = state.reportDraft.contact
             return .none
         case let .showReportWizard(value):
             state.showReportWizard = value
             return .none
         case .reportSaved:
-            state.reportDraft = Report(images: .init(), contact: state.contact, date: Date.init)
+            state.reportDraft = Report(images: .init(), contact: state.settings.contact, date: Date.init)
             return .none
         }
     })
