a2da2894fae1f09466021dfcab5aec0657114bed
Malte Bünz <maltebuenz@posteo.de>
2021-04-08 20:44:02 +0200

    add uiapplicationclient

---
+++ weg-li/Services/UIApplicationClient.swift
@@ -0,0 +1,21 @@
+// Created for weg-li in 2021.
+
+import ComposableArchitecture
+import UIKit
+
+struct UIApplicationClient {
+    var open: (URL, [UIApplication.OpenExternalURLOptionsKey: Any]) -> Effect<Bool, Never>
+    var openSettingsURLString: () -> String
+}
+
+extension UIApplicationClient {
+    public static let live = Self(
+        open: { url, options in
+            .future { callback in
+                UIApplication.shared.open(url, options: options) { bool in
+                    callback(.success(bool))
+                }
+            }
+        },
+        openSettingsURLString: { UIApplication.openSettingsURLString })
+}
