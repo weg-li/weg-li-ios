d0a55b176a75cdb5325c4e521f974ec25c818597
Malte Bünz <maltebuenz@posteo.de>
2022-02-14 11:28:14 +0100

    migrate UIApplicationClient

---
+++ WegliKit/Sources/UIApplicationClient/Live.swift
@@ -0,0 +1,15 @@
+import ComposableArchitecture
+import UIKit.UIApplication
+
+public extension UIApplicationClient {
+  static let live = Self(
+    open: { url, options in
+        .future { callback in
+          UIApplication.shared.open(url, options: options) { bool in
+            callback(.success(bool))
+          }
+        }
+    },
+    openSettingsURLString: { UIApplication.openSettingsURLString }
+  )
+}
---
+++ WegliKit/Sources/UIApplicationClient/Mock.swift
@@ -0,0 +1,9 @@
+import ComposableArchitecture
+import UIKit.UIApplication
+
+public extension UIApplicationClient {
+  static let noop = Self(
+      open: { _, _ in .none },
+      openSettingsURLString: { "" }
+  )
+}
