10cd45338df30d4bcc9bf5ecb7fbd064b57cbb87
Malte Bünz <maltebuenz@posteo.de>
2022-05-22 14:47:50 +0200

    inject notice

--- WegliKit/Sources/AppFeature/NoticeView.swift
+++ WegliKit/Sources/AppFeature/NoticeView.swift
@@ -3,24 +3,18 @@
 import DescriptionFeature
 import Helper
 import ReportFeature
+import SharedModels
 import Styleguide
 import SwiftUI
 
 public struct NoticeView: View {
-  public let createdAt: Date
-  public let brand: String
-  public let color: String?
-  public let licensePlateNumber: String
-  public let duration: String
-  public let interval: String?
-  public let charge: String
-  public let status: String
+ public let notice: Notice
   
   public var body: some View {
     ZStack {
       HStack {
         VStack(alignment: .leading) {
-          Text(createdAt.humandReadableDate)
+          Text(notice.createdAt.humandReadableDate)
             .fontWeight(.bold)
             .font(.title)
             .padding(.bottom, .grid(1))
@@ -31,7 +25,7 @@ public struct NoticeView: View {
               .accessibility(hidden: true)
             
             VStack(alignment: .leading, spacing: .grid(1)) {
-              Text("Status: __\(status)__")
+              Text("Status: __\(notice.status)__")
             }
             .font(.body)
           }
@@ -45,14 +39,14 @@ public struct NoticeView: View {
               .accessibility(hidden: true)
             
             VStack(alignment: .leading, spacing: .grid(1)) {
-              Text(verbatim: licensePlateNumber)
+              Text(verbatim: notice.registration)
                 .font(.custom(FontName.nummernschild.rawValue, size: 22, relativeTo: .body))
                 .foregroundColor(Color(.label))
                 .textCase(.uppercase)
               
               VStack(alignment: .leading) {
-                Text("Marke: __\(brand)__")
-                if let color = color {
+                Text("Marke: __\(notice.brand)__")
+                if let color = notice.displayColor {
                   Text("Farbe: __\(color)__")
                 }
               }
@@ -70,12 +64,14 @@ public struct NoticeView: View {
           
             VStack(alignment: .leading, spacing: .grid(1)) {
               VStack(alignment: .leading) {
-                Text(duration)
-                if let interval = interval {
+                if let time = notice.time?.description {
+                  Text(time)
+                }
+                if let interval = notice.interval {
                   Text(interval)
                 }
               }
-              Text(charge)
+              Text(notice.charge)
             }
             .font(.body)
           }
@@ -112,16 +108,7 @@ public struct NoticeView: View {
 struct NoticeView_Previews: PreviewProvider {
   static var previews: some View {
     Preview {
-      NoticeView(
-        createdAt: .distantFuture,
-        brand: "Opel",
-        color: "Grau",
-        licensePlateNumber: "B-ER-2021",
-        duration: "15 Minuten",
-        interval: "234234",
-        charge: "Parken auf dem Gehweg",
-        status: "Offen"
-      )
+      NoticeView(notice: .mock)
     }
   }
 }
