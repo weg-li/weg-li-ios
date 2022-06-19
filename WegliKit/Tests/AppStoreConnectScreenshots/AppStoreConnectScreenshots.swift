5bb25ed133cf41b73362ceaaf831bdc7ab1f12d8
Malte Bünz <maltebuenz@posteo.de>
2022-06-19 21:53:22 +0200

    update storescreenshot tests

--- WegliKit/Tests/AppStoreConnectScreenshots/AppStoreConnectScreenshots.swift
+++ WegliKit/Tests/AppStoreConnectScreenshots/AppStoreConnectScreenshots.swift
@@ -30,14 +30,16 @@ class AppStoreConnectScreenshots: XCTestCase {
     super.tearDown()
   }
   
-  func test_AppViewScreenShot() {
-    let appState = AppState(
-      settings: .init(contact: .preview),
-      reports: [
-        .preview
-      ],
-      showReportWizard: false
+  func test_noticesTab() {
+    var appState = AppState(
+      settings: .init(
+        accountSettingsState: .init(accountSettings: .init(apiToken: "")),
+        contact: .preview,
+        userSettings: .init()
+      ),
+      notices: .results([.init(.preview), .report2])
     )
+    appState.selectedTab = .notices
     let view = AppView(
       store: .init(
         initialState: appState,
@@ -48,7 +50,42 @@ class AppStoreConnectScreenshots: XCTestCase {
     
     assertAppStoreSnapshots(
       view: AnyView(view),
-      description: { Text("Behalte deine Anzeigen im Blick").foregroundColor(Color.black.opacity(0.4)) },
+      description: { Text("Behalte deine Meldungen im Blick").foregroundColor(Color.black.opacity(0.4)) },
+      backgroundColor: .wegliBlue,
+      colorScheme: .light
+    )
+  }
+  
+  func test_newNotice() {
+    var appState = AppState(
+      settings: .init(
+        accountSettingsState: .init(accountSettings: .init(apiToken: "")),
+        contact: .preview,
+        userSettings: .init()
+      ),
+      notices: .results([.init(.preview), .report2])
+    )
+    appState.selectedTab = .notice
+    appState.reportDraft = .preview
+    appState.reportDraft.images = .init(
+      alert: nil,
+      showImagePicker: false,
+      storedPhotos: [PickerImageResult(id: "1", uiImage: UIImage(named: "homer", in: .module, with: nil)!, imageUrl: nil)],
+      coordinateFromImagePicker: nil,
+      dateFromImagePicker: nil
+    )
+    
+    let view = AppView(
+      store: .init(
+        initialState: appState,
+        reducer: .empty,
+        environment: ()
+      )
+    )
+    
+    assertAppStoreSnapshots(
+      view: AnyView(view),
+      description: { Text("Erstelle eine Meldung mit Hilfe der App").foregroundColor(Color.black.opacity(0.4)) },
       backgroundColor: .wegliBlue,
       colorScheme: .light
     )
@@ -65,9 +102,41 @@ class AppStoreConnectScreenshots: XCTestCase {
     
     assertAppStoreSnapshots(
       view: AnyView(NavigationView(content: { view }).navigationViewStyle(StackNavigationViewStyle())),
-      description: { Text("Erstelle eine Anzeige mit Hilfe der App") },
+      description: { Text("Beschreibe die Meldung") },
       backgroundColor: .wegliBlue,
       colorScheme: .light
     )
+  }
+}
+
+
+extension Notice {
+  static var report2: Notice {
+    let report = ReportState(
+      uuid: UUID.init,
+      images: .init(
+        showImagePicker: false,
+        storedPhotos: [PickerImageResult(uiImage: UIImage(named: "homer", in: .module, with: nil)!)] // swiftlint:disable:this force_unwrapping
+      ),
+      contactState: .preview,
+      district: District(
+        name: "Berlin",
+        zip: "12435",
+        email: "mail@ba-berlin-treptow.de",
+        latitude: 53.53,
+        longitude: 13.13,
+        personalEmail: true
+      ),
+      date: { Date(timeIntervalSince1970: 1_580_624_207) },
+      description: .init(
+        licensePlateNumber: "B-MX-231",
+        selectedColor: 6,
+        selectedBrand: .init("Ente"),
+        selectedDuration: 2,
+        selectedCharge: .init(id: "1", text: "Parken auf dem Radweg", isFavorite: true, isSelected: false),
+        blockedOthers: true
+      )
+    )
+    return .init(report)
   }
 }
