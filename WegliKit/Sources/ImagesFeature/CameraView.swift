19d9104e326df3a88b14376dc488b9a51b0c902d
Malte Bünz <maltebuenz@posteo.de>
2022-08-08 13:06:53 +0200

    format code (#65)

--- WegliKit/Sources/ImagesFeature/CameraView.swift
+++ WegliKit/Sources/ImagesFeature/CameraView.swift
@@ -1,9 +1,8 @@
 import PhotosUI
-import SwiftUI
 import SharedModels
+import SwiftUI
 
 public struct CameraView: UIViewControllerRepresentable {
-
   @Binding var isPresented: Bool
   @Binding var pickerResult: [PickerImageResult?]
 
@@ -22,20 +21,20 @@ public struct CameraView: UIViewControllerRepresentable {
     Coordinator(self)
   }
 
-  final public class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+  public final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var parent: CameraView
 
     init(_ parent: CameraView) {
       self.parent = parent
     }
 
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
       defer {
         parent.isPresented = false
       }
       guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
-      let imageData = image.jpegData(compressionQuality: 1) else {
+            let imageData = image.jpegData(compressionQuality: 1)
+      else {
         debugPrint("originalImage info from ImagePickerController could not be casted to UIImage")
         return
       }
@@ -45,7 +44,8 @@ public struct CameraView: UIViewControllerRepresentable {
 
       var coordinate: CoordinateRegion.Coordinate?
       if let asset: PHAsset = info[UIImagePickerController.InfoKey.phAsset] as? PHAsset,
-         let imageCoordinate = asset.location?.coordinate {
+         let imageCoordinate = asset.location?.coordinate
+      {
         coordinate = .init(imageCoordinate)
       }
 
